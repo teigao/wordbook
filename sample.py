ef2752e187c5d7e01097e15758588947e2659267
teigao <yiqgao@outlook.com>
2019-12-20 21:22:17 +0800

    change

--- sample.py
+++ sample.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import wordbook
 import webbrowser
-dataset = pd.DataFrame({"word":"simple","freq":"W1"},{"word":"hard","freq":"W2"},{"word":"example","freq":"W3"})
+dataset = pd.DataFrame({"word": "simple", "freq": "W1"}, {
+                       "word": "hard", "freq": "W2"}, {"word": "example", "freq": "W3"})
 print(wordbook.generate_html(dataset))
