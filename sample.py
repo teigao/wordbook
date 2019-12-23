1028957561dced7784c8220c92ebd76c39288d71
teigao <yiqgao@outlook.com>
2019-12-23 13:41:05 +0800

    change source

--- sample.py
+++ sample.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import wordbook
-import webbrowser
+
 dataset = pd.DataFrame({"word": "simple", "freq": "W1"}, {
                        "word": "hard", "freq": "W2"}, {"word": "example", "freq": "W3"})
+
 print(wordbook.generate_html(dataset))
--- wordbook/__init__.py
+++ wordbook/__init__.py
@@ -11,14 +11,19 @@ it will provides a html string.
 __author__ = 'Teige Gao'
 
 from wordbook import get_dic
+import os
+import webbrowser
+
+_current_path = os.path.dirname(__file__)
+_root_folder = _current_path+"/_resource/"
 
 
-def query_word(word, fre = ''):
+def query_word(word, fre=''):
     """
     This function will return a defination of a word.
     """
     word_item = '<div class = "word_ite"><span class = "word_def_w">' + \
-        word + '</span><span class = "lon_def"> ' + fre + '</span></div>'
+        word + '</span><span class = "lon_def"> ' + str(fre) + '</span></div>'
     if word in get_dic.voc_easy.index:
         voc_easy = '<div class = "explaincard_content" style="font-style:italic">' + \
             get_dic.voc_easy.loc[word] + '</div>'
@@ -40,12 +45,12 @@ def query_word(word, fre = ''):
     else:
         easy_und = ''
     if voc_easy != '' or voc_more != '':
-        voc = '<div class = "explaincard"><div class = "explaincard_title">&nbsp&nbspExplation E-E</div>' + \
+        voc = '<div class = "explaincard"><div class = "explaincard_title">&nbsp;&nbsp;Explation E-E</div>' + \
             voc_easy + voc_more + '</div>'
     else:
         voc = ''
     if cn_define_fre != '' or easy_und != '':
-        vvv = '<div class = "explaincard"><div class = "explaincard_title">&nbsp&nbspExplation C-E</div>' + \
+        vvv = '<div class = "explaincard"><div class = "explaincard_title">&nbsp;&nbsp;Explation C-E</div>' + \
             cn_define_fre + easy_und + '</div>'
     else:
         vvv = ''
@@ -60,182 +65,26 @@ def generate_html(dataset):
     """
     This function will return a html of wordbook quried by the dataset.
     """
+    html_style = open(
+        _root_folder + 'style.css', 'r', encoding='UTF-8').read()
+    out_html = open(_root_folder + 'wordbook.html', 'w', encoding='UTF-8')
     temp = dataset.apply(lambda x: query_word(
         x['word'], x['freq']), axis=1).tolist()
     result = '''<html>
     <head>
         <style>
-            body {
-                text-align: left;
-                color: #000000;
-                padding: 0px;
-                margin: 0px;
-            }
-            .word_ite {font-size: 2em;}
-            .lon_def {font-size: 0.5em;}
-            .explaincard {
-                margin:0px; 
-                padding-top: 15px;
-                padding-left: 15px;
-                padding-right: 15px;
-                padding-bottom: 10px;
-            }
-            .explaincard_title {
-                color: #000000;
-                font-weight: bold;
-                border-left:5px solid #1282e2; 
-                margin-bottom: 10px;
-            }
-            .explaincard_content {
-                margin-bottom: 5px;
-                line-height: 1.5em;
-                margin-top: 5px;
-                color: #000000;
-            }
-            .hr_more_define {
-                border:none;
-                border-top: 1px solid #B3E5FC;
-            }
-            .hw {
-                display:none;
-            }
-            .pt {
-                display:none;
-                }
-            .word-frequency {
-                margin-bottom:3px;
-                }
-            .coca2 {
-                margin-top:5px;
-                }
-            .coca_desc {
-                margin-top:3px;
-                }
-            .pos_v {
-                display: inline;
-                vertical-align:middle;
-                padding:1px 2px 1px 2px;
-                text-align: center;
-                border-radius: 5px;
-                margin: 0px 3px 0px 0px;
-                font-size:12px;
-                color: #ffffff;
-                background: #4CAF50;
-            }
-            .pos_n {
-                display: inline;
-                vertical-align:middle;
-                padding:1px 2px 1px 2px;
-                text-align: center;
-                border-radius: 5px;
-                margin: 0px 3px 0px 0px;
-                font-size:12px;
-                color: #ffffff;
-                background: #03A9F4;
-            }
-            .pos_j {
-                display: inline;
-                vertical-align:middle;
-                padding:1px 2px 1px 2px;
-                text-align: center;
-                border-radius: 5px;
-                margin: 0px 3px 0px 0px;
-                font-size:12px;
-                color: #ffffff;
-                background: #f9320c;
-            }
-            .pos_r {
-                display: inline;
-                vertical-align:middle;
-                padding:1px 2px 1px 2px;
-                text-align: center;
-                border-radius: 5px;
-                margin: 0px 3px 0px 0px;
-                font-size:12px;
-                color: #ffffff;
-                background: #7200da;
-            }
-            .pos_u {
-                display: inline;
-                vertical-align:middle;
-                padding:1px 2px 1px 2px;
-                text-align: center;
-                border-radius: 5px;
-                margin: 0px 3px 0px 0px;
-                font-size:12px;
-                color: #ffffff;
-                background: #ff5f2e;
-            }
-            .label {
-                padding:1px 2px 1px 2px;
-                text-align: center;
-                border-radius: 5px;
-                margin: 3px;
-                text-transform:capitalize;
-                font-size:12px;
-                border:1px solid;
-            }    
-            .label-primary {
-                color: rgb(18,130, 226);
-                border-color: rgb(18,130,226);
-            }
-            .label-success {
-            color: rgb(18,130, 226);
-                border-color: rgb(18,130,226);
-            }
-            .label-info {
-            color: rgb(18,130, 226);
-                border-color: rgb(18,130,226);
-            }
-            .label-warning {
-            color: rgb(18,130, 226);
-                border-color: rgb(18,130,226);
-            }
-            .rank {
-                font-style: italic;
-            }
-            .total {
-                font-style: italic;
-            }
-            .rank:before {
-                content: " RANK ";
-                font-weight: normal;
-                font-style: normal;
-            }
-            .rank:after {
-                content: " FREQ ";
-                font-weight: normal;
-                font-style: normal;
-            }
-            .table {
-                display: none;
-            }
-            .pos {
-                background: #4CAF50;
-                color:#ffffff;
-                border-radius: 2px;
-                padding: 1.5px;
-                padding-top: 0px;
-                padding-bottom: 0px;
-                font-size: 0.9em;
-            }
-            .pos:after {
-                content: ".";
-            }
-            .rank {
-                display: inline;
-                vertical-align:middle;
-            }
-            .total {
-                display: inline;
-                vertical-align:middle;
-            }
+        ''' + html_style + '''
         </style>
     </head>
-    <body>''' + "".join(temp) + '''    
+    <body>''' + "".join(s for s in temp if s is not None) + '''    
     </body>
 </html>'''
-    return result
+    out_html.write(result)
+    out_html.close()
+    html_path = 'file:///' + _root_folder + 'wordbook.html'
+    webbrowser.open_new_tab(html_path)
+    message = "The html has been generated"
+    return message
 
 
 if __name__ == '__main__':
